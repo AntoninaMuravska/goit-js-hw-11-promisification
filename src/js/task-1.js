174e5194bab7421b2b11762fb3c2506947693063
Antonina Muravska <at.tonya@gmail.com>
2020-12-21 11:35:30 +0200

    fix task-1

--- src/js/task-1.js
+++ src/js/task-1.js
@@ -2,9 +2,11 @@
 //Значением исполнившегося промиса должно быть то кол - во миллисекунд которое передали во время вызова функции delay.
 
 const delay = ms => {
-    return new Promise(resolve => {
-    setTimeout(() => resolve(ms))
-}, ms)
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
 };
 
 const logger = time => console.log(`Resolved after ${time}ms`);
